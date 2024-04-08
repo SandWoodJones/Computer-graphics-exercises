0981c8a3383a2f11ab4a23676cd3736c7b9738b4
SandWood Jones <sandwoodjones@outlook.com>
2024-04-08 00:04:45 -0300

    added opengl template and started first exercise

---
+++ 2Triangulos/src/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+
+const int   gW_Width = 640;
+const int   gW_Height = 360;
+const char* gW_Title = "project_name";
+
+int main(void) {
+	// init glfw
+	if (!glfwInit()) {
+		std::cerr << "GLFW initialization failed with error code " << glfwGetError(NULL) << std::endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *win = glfwCreateWindow(gW_Width, gW_Height, gW_Title, NULL, NULL);
+	if (!win) {
+		std::cerr << "GLFW window creating failed with error code " << glfwGetError(NULL) << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(win);
+
+	// init glew
+	glewExperimental = GL_TRUE;
+	if (GLenum err = glewInit() != GLEW_OK) {
+		std::cerr << "GLEW initialization failed with error code " << err << std::endl;
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(win)) {
+		glfwPollEvents();
+
+		glfwSwapBuffers(win);
+	}
+
+	return 0;
+}
