c9147584022a916b2229a2e8bffe344a9c6f66ec
Raul Rodrigues de Oliveira <sandwoodjones@outlook.com>
2024-04-12 18:54:03 -0300

    ex4 done

---
+++ sinc/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+
+#include <shaderClass.h>
+
+const int   gW_Width = 640;
+const int   gW_Height = 360;
+const char* gW_Title = "sinc function";
+
+void generateVerts(GLfloat arr[]);
+
+int main(void) {
+	// init glfw
+	if (!glfwInit()) {
+		std::cerr << "GLFW initialization failed with error code " << glfwGetError(NULL) << std::endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* win = glfwCreateWindow(gW_Width, gW_Height, gW_Title, NULL, NULL);
+	if (!win) {
+		std::cerr << "GLFW window creating failed with error code " << glfwGetError(NULL) << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(win);
+
+	// init glew
+	glewExperimental = GL_TRUE;
+	if (GLenum err = glewInit() != GLEW_OK) {
+		std::cerr << "GLEW initialization failed with error code " << err << std::endl;
+		return -1;
+	}
+
+	// populate array with sinc values
+	GLfloat verts[162] = { 0 };
+	generateVerts(verts);
+
+	GLuint VBO, VAO;
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(0);
+
+	Shader defaultShader = Shader("default.vert", NULL);
+
+	glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+	glPointSize(3);
+
+	while (!glfwWindowShouldClose(win)) {
+		glfwPollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		defaultShader.Activate();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_LINE_STRIP, 0, 81);
+		glBindVertexArray(0);
+
+		glfwSwapBuffers(win);
+	}
+
+	defaultShader.Delete();
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glfwTerminate();
+
+	return 0;
+}
+
+void generateVerts(GLfloat arr[]) {
+	GLuint i = 0;
+	for (GLfloat x = -10.0f; x <= 10.0f; x += 0.25f) {
+		arr[i] = x;
+
+		GLfloat result = glm::sin(x) / x;
+		if (isnan(result)) result = 1.0f; // replaces result with 1 when x = 0 to avoid problems
+		arr[i + 1] = result;
+
+		i += 2;
+	}
+}
